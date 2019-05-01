41cbaf99401028f33648c7eab7aa55decd8c7f83
Thomas Buck <xythobuz@xythobuz.de>
2019-05-01 14:02:24 +0200

    Updated to PHP 7 postgres lib

--- config.example.php
+++ config.example.php
@@ -1,6 +1,7 @@
 <?php
 
 $sql_host = 'localhost';
+$sql_port = '5432';
 $sql_username = 'root';
 $sql_password = '';
 $sql_database = 'weight-test';
--- weight.php
+++ weight.php
@@ -46,9 +46,6 @@
 function print_json_error($s) {
     echo '{';
     echo '  "error": "' . $s;
-    if (mysql_errno()) {
-        echo ' (' . mysql_error() . ')';
-    }
     echo '"';
     echo '}';
 }
@@ -61,9 +58,6 @@ function print_html($s) {
     echo '    </head>';
     echo '    <body>';
     echo '        <p>' . $s . '</p>';
-    if (mysql_errno()) {
-        echo '<p>MySQL Error: "' . mysql_error() . '"</p>';
-    }
     echo '        <a href="index.html">Back to Main-Page...</a>';
     echo '    </body>';
     echo '</html>';
@@ -80,6 +74,7 @@ function print_error($s) {
 include('config.php');
 
 if ((!isset($sql_host))
+        || (!isset($sql_port))
         || (!isset($sql_username))
         || (!isset($sql_password))
         || (!isset($sql_database))) {
@@ -87,32 +82,32 @@ if ((!isset($sql_host))
     exit(1);
 }
 
-$db = mysql_connect($sql_host, $sql_username, $sql_password);
-mysql_select_db($sql_database);
-if (mysql_errno()) {
-    print_error('Database Error');
+$conn_string = 'host=' . $sql_host . ' port=' . $sql_port . ' dbname=' . $sql_database;
+$conn_string = $conn_string . ' user=' . $sql_username . ' password=' . $sql_password;
+$db = pg_connect($conn_string);
+if (!$db) {
+    print_error('Database Error: ' . pg_last_error($db));
     exit(1);
 }
 
 $sql = 'CREATE TABLE IF NOT EXISTS weight_users (';
-$sql .= 'id int NOT NULL AUTO_INCREMENT,';
-$sql .= 'name varchar(255),';
-$sql .= 'PRIMARY KEY (id)';
+$sql .= 'id SERIAL,';
+$sql .= 'name varchar';
 $sql .= ');';
-$result = mysql_query($sql);
+$result = pg_query($db, $sql);
 if (!$result) {
-    print_error('Error (re-) creating database table for users!');
+    print_error('Error (re-) creating database table for users: ' . pg_result_error($result));
     exit(1);
 }
 
 $sql = 'CREATE TABLE IF NOT EXISTS weight_data (';
 $sql .= 'id int NOT NULL,';
-$sql .= 'date DATETIME,';
+$sql .= 'date TIMESTAMP,';
 $sql .= 'weight DECIMAL(5,2)';
 $sql .= ');';
-$result = mysql_query($sql);
+$result = pg_query($db, $sql);
 if (!$result) {
-    print_error('Error (re-) creating database table for weights!');
+    print_error('Error (re-) creating database table for weights: ' . pg_result_error($result));
     exit(1);
 }
 
@@ -120,9 +115,9 @@ if (isset($_POST['username'])
         && isset($_POST['date'])
         && isset($_POST['time'])
         && isset($_POST['weight'])) {
-    $sql = 'INSERT INTO weight_data(id, date, weight) VALUES (';
-    $sql .= mysql_real_escape_string(str_replace('user_', '', $_POST['username']));
-    $sql .= ', ';
+    $sql = 'INSERT INTO weight_data(id, date, weight) VALUES (\'';
+    $sql .= pg_escape_string(str_replace('user_', '', $_POST['username']));
+    $sql .= '\', ';
 
     $datetime = $_POST['date'] . $_POST['time'];
     $timestamp = strtotime($datetime);
@@ -131,23 +126,23 @@ if (isset($_POST['username'])
         exit(1);
     }
     $mysqltime = date("Y-m-d H:i:s", $timestamp);
-    $sql .= '"' . $mysqltime . '", ';
+    $sql .= '\'' . $mysqltime . '\', \'';
 
-    $sql .= mysql_real_escape_string($_POST['weight']) . ')';
+    $sql .= pg_escape_string($_POST['weight']) . '\')';
 
-    $result = mysql_query($sql);
+    $result = pg_query($db, $sql);
     if (!$result) {
-        print_error('Error adding new data for user "' . $_POST['username'] . '" to DB! ' . $sql);
+        print_error('Error adding new data for user "' . $_POST['username'] . '" to DB: ' . $pg_result_error($result));
     } else {
         print_error('Added new data for user "' . $_POST['username'] . '" to DB!');
     }
 } else if (isset($_POST['username'])) {
-    $sql = 'INSERT INTO weight_users(name) VALUES ("';
-    $sql .= mysql_real_escape_string($_POST['username']);
-    $sql .= '")';
-    $result = mysql_query($sql);
+    $sql = 'INSERT INTO weight_users(name) VALUES (\'';
+    $sql .= pg_escape_string($_POST['username']);
+    $sql .= '\')';
+    $result = pg_query($db, $sql);
     if (!$result) {
-        print_error('Error adding new user "' . $_POST['username'] . '" to DB!');
+        print_error('Error adding new user "' . $_POST['username'] . '" to DB: ' . pg_result_error($result));
     } else {
         print_error('Added new user "' . $_POST['username'] . '" to DB!');
     }
@@ -204,25 +199,25 @@ if (isset($_POST['username'])
 EOF;
 } else {
     $sql = 'SELECT id, name FROM weight_users ORDER BY id ASC';
-    $result = mysql_query($sql);
+    $result = pg_query($db, $sql);
     if (!$result) {
-        print_error('Error fetching users from database!');
+        print_error('Error fetching users from database: ' . pg_result_error($result));
         exit(1);
     }
     $data = array();
-    while ($row = mysql_fetch_array($result)) {
+    while ($row = pg_fetch_assoc($result)) {
         $sql2 = 'SELECT date, weight FROM weight_data ';
-        $sql2 .= 'WHERE id = "' . $row['id'] . '" ORDER BY date ASC';
-        $result2 = mysql_query($sql2);
+        $sql2 .= 'WHERE id = \'' . $row['id'] . '\' ORDER BY date ASC';
+        $result2 = pg_query($db, $sql2);
         if (!$result2) {
-            print_error('Error fetching data for user ' . $row['id'] . ' "' . $row['name'] . '"!');
+            print_error('Error fetching data for user ' . $row['id'] . ' "' . $row['name'] . '": ' . pg_result_error($result2));
             exit(1);
         }
         $cur = array();
         $cur['name'] = $row['name'];
         $cur['id'] = $row['id'];
         $cur['data'] = array();
-        while ($row2 = mysql_fetch_array($result2)) {
+        while ($row2 = pg_fetch_assoc($result2)) {
             $elem = array();
             $elem['date'] = $row2['date'];
             $elem['weight'] = $row2['weight'];
